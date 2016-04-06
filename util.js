6acf7d4b697e578f51482de010a0d8531801761f
Casey Thomas <cthomas@azavea.com>
2016-04-06 11:14:35 -0400

    Add polyfill for String.endsWith
    * This method is not available in IE and was preventing
    the plugin from loading.
    
    Refs https://github.com/CoastalResilienceNetwork/GeositeFramework/issues/626

--- util.js
+++ util.js
@@ -1,6 +1,20 @@
 define(['underscore'], function(_) {
     "use strict";
 
+    // String.endsWith Polyfill for IE.
+    // Source: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+    if (!String.prototype.endsWith) {
+      String.prototype.endsWith = function(searchString, position) {
+          var subjectString = this.toString();
+          if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+          }
+          position -= searchString.length;
+          var lastIndex = subjectString.indexOf(searchString, position);
+          return lastIndex !== -1 && lastIndex === position;
+      };
+    }
+
     // Differs from the underscore `find` in that this version will return
     // the first non-falsy result, instead of an item from `collection`.
     // Synonymous to: _.filter(_.map(collection, predicate))[0]
